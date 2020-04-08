fe71d5ceef5c8541bc190dcbb7faf803dfa9ed68
Sanmoh Hombal <devsanmoh@gmail.com>
2020-04-09 05:01:13 +0530

    Recursive Insertion Sort

--- Sort-Algorithms/InsertionSort.py
+++ Sort-Algorithms/InsertionSort.py
@@ -9,7 +9,22 @@ def insertionSort(arr):
         arr[j + 1] = key
 
 
+def insertionSortRecursive(arr, n):
+    if n <= 1:
+        return
+
+    insertionSortRecursive(arr, n - 1)
+    last = arr[n - 1]
+    j = n - 2
+
+    while (j >= 0 and arr[j] > last):
+        arr[j + 1] = arr[j]
+        j = j - 1
+
+    arr[j + 1] = last
+
+
 arr = [12, 11, 13, 5, 6]
-insertionSort(arr)
+insertionSortRecursive(arr, len(arr))
 for i in range(len(arr)):
     print ("% d" % arr[i])
